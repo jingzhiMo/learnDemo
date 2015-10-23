4159f6fc50caeaa286146165dd36bb62df6e30fb
jingzhimo <mojingzhi.mail@gmail.com>
2015-10-23 16:45:40 +0800

    # 编写了主页的基本模块，使用了jade的继承，基本可以看到

--- node_imooc/app.js
+++ node_imooc/app.js
@@ -1,9 +1,11 @@
 var express = require('express'),
+	path = require('path'),
 	port = 3000;
 
 var app = express();
 app.set('views', './views/pages');
 app.set('view engine', 'jade');
+app.use(express.static(path.join(__dirname, 'bower_components')));
 app.listen(port);
 console.log('server starting');
 
@@ -11,7 +13,23 @@ console.log('server starting');
 // 1. 首页
 app.get('/', function(req, res) {
 	res.render('index', {
-		title: '首页'
+		movies: [
+			{
+				'_id': 1,
+				'title': '电影名1',
+				'poster': 'http://i1.sinaimg.cn/gm/2014/0415/U10229P115DT20140415134404.jpg'
+			},
+			{
+				'_id': 2,
+				'title': '电影名2',
+				'poster': 'http://i1.sinaimg.cn/gm/2014/0415/U10229P115DT20140415134404.jpg'
+			},
+			{
+				'_id': 3,
+				'title': '电影名3',
+				'poster': 'http://i1.sinaimg.cn/gm/2014/0415/U10229P115DT20140415134404.jpg'
+			}
+		]
 	});
 });
 // 2.列表页
