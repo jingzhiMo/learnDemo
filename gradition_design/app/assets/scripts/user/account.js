d8eddb8dbb7ebc5e869d4dd19fd8e9ceff077efa
jingzhimo <mojingzhi.mail@gmail.com>
2016-02-22 23:02:39 +0800

    输入手机号码的简单验证

--- gradition_design/app/assets/scripts/user/account.js
+++ gradition_design/app/assets/scripts/user/account.js
@@ -1,7 +1,17 @@
 var accountMD = angular.module('accountMD', []);
 
 accountMD.controller('accountCtrl', ['$scope', function($scope){
-	$scope.click = function() {
-		alert('haha');
+	var tips = '该账号已经比人使用了';
+
+	$scope.noticTips = '';
+	$scope.checkPhone = function(phoneNum) {
+		if ( /1\d{10}/.test(phoneNum) ) { // 简单判断手机号码
+			console.log(true);
+			$scope.noticTips = '';
+		}
+		else {
+			$scope.noticTips = '手机号码不正确或者已经被注册';
+			console.log(false);
+		}
 	};
 }]);
