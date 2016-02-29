16ca968b28421342481d68d14c41ee33948cd41c
jingzhimo <mojingzhi.mail@gmail.com>
2016-02-29 23:17:16 +0800

    服务器开启成功，express 成功连接到 mongondb

--- gradition_design/server.js
+++ gradition_design/server.js
@@ -1,15 +1,25 @@
-var express  = require('express'),
-	mongoose = require('mongoose'),
-	config   = require('./config');
+var express = require('express'),
+	mongo   = require('mongoose'),
+	config  = require('./config');
 
 var app = express();
+
+// 部署前端资源
+app.use(express.static('app'));
+
+// 页面路由
 app.get('/', function(req, res) {
-	res.send('hello world');
+	res.send(JSON.stringify(config));
+})
+.get('/init', function(req, res) {
+	// TODO
+	res.send(config.db.mongodb);
 });
 
 var server = app.listen(3000, function() {
-	// var host = server.address().address;
-	// var port = server.address().port;
 
-	console.log('server is listening');
+	// 连接数据库
+	mongo.connect(config.db.mongodb);
+	
+	console.log('server is running');
 });
