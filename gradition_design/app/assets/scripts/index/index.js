3b7c4c6df40d86fc1629b292c341c189b7b21a01
jingzhiMo <mojingzhi.mail@gmail.com>
2016-03-03 15:08:13 +0800

    左右滑动事件完成

--- gradition_design/app/assets/scripts/index/index.js
+++ gradition_design/app/assets/scripts/index/index.js
@@ -1,9 +1,9 @@
-var indexApp = angular.module('indexApp', ['pageMD']);
+var indexApp = angular.module('indexApp', ['pageMD', 'eventMD']);
 
-indexApp.controller('indexCtrl', ['$scope', '$http', function($scope, $http) {
+indexApp.controller('indexCtrl', ['$scope', '$http', 'event', function($scope, $http, event) {
 
 }])
-.directive('catalog', function() {
+.directive('catalog', ['event', function(event) {
 
 	var positionX = 0;
 	return {
@@ -13,30 +13,32 @@ indexApp.controller('indexCtrl', ['$scope', '$http', function($scope, $http) {
 		templateUrl: '/assets/tmpl/catalog.html',
 		link: function(scope, ele, attr) {
 
+			var ul = ele.children()[0];
+			event.move(ul, ul.offsetWidth / 2, 2);
 			// ele.children()[0].addEventListener('touchstart', function() {
 			// 	// console.log(ele);
 			// 	// console.log(scope);
 			// 	console.log('haha');
 			// }, false);
 
-			ele.bind('touchstart', function(ev) {
-				positionX = ev.touches[0].pageX;
-				console.log(ele);
-				console.log(attr);
-				console.log('start');
-				console.log(ev);
-			})
-			.bind('touchmove', function(ev) {
-				// var distanceX = ev.touches[0].pageX - positionX;
+			// ele.bind('touchstart', function(ev) {
+			// 	positionX = ev.touches[0].pageX;
+			// 	console.log(ele);
+			// 	console.log(attr);
+			// 	console.log('start');
+			// 	console.log(ev);
+			// })
+			// .bind('touchmove', function(ev) {
+			// 	// var distanceX = ev.touches[0].pageX - positionX;
 
-				// ele.
+			// 	// ele.
 
-				console.log(ev.touches[0].pageX);
-				console.log('move');
-			})
-			.bind('touchend', function(ev) {
-				console.log('end');
-			});
+			// 	console.log(ev.touches[0].pageX);
+			// 	console.log('move');
+			// })
+			// .bind('touchend', function(ev) {
+			// 	console.log('end');
+			// });
 		}
 	};
-});
+}]);
--- gradition_design/app/assets/scripts/public/util/event.js
+++ gradition_design/app/assets/scripts/public/util/event.js
@@ -18,25 +18,45 @@ angular.module('eventMD', [])
 			 *  =move
 			 *  @about    封装touch move 事件
 			 *
-			 *  @param    {object}  ele  需要绑定事件的 dom 元素
+			 *  @param    {object}  ele   需要绑定事件的 dom 元素
+			 *  @param    {number}  width 每滑动一屏的宽度
+			 *  @param    {number}  count 一共有多少屏
 			 */
-			move: function(ele) {
-				var startX = 0;
+			move: function(ele, width, count) {
+				var startX = 0, moveX, distanceX,
+					eleStyle = ele.style,
+					index = 1,
+					currX = 0;
 
 				ele.addEventListener('touchstart', function(ev) {
 					startX = ev.touches[0].pageX;
 				}, false);
 
 				ele.addEventListener('touchmove', throttle(function(ev) {
-						var moveX     = ev.touches[0].pageX,
-							distanceX = moveX - startX;
-
-						ele.style = 'transform: (' + distanceX + 'px, 0px, 0px)';
-					}, 50, 100), 
+						distanceX = ev.touches[0].pageX - startX;
+						setStyle(ele, currX + distanceX);
+					}, 5, 10), 
 				false);
 
 				ele.addEventListener('touchend', function(ev) {
-					// TODO
+					clearTimeout(timer);
+					if ( distanceX < 0 ) { // 向左滑动
+
+						if ( index < count ) {
+							currX -= width;
+							index++;
+						}
+						setStyle(ele, currX, 300);
+					}
+					else { // 向右滑动
+
+						if ( index !== 1 ) {
+							currX += width;
+							index--;
+						}
+						setStyle(ele, currX, 300);
+					}
+					startX = currX;
 				}, false);
 			}
 		};
@@ -50,9 +70,10 @@ angular.module('eventMD', [])
  *  @param  {number}   delay   延迟多少时间执行，毫秒为单位
  *  @param  {number}   mustRun 超过这个时间，一定执行函数，毫秒为单位
  */
+var timer = null;
 function throttle(fn, delay, mustRun) {
-	var timer   = null,
-		t_start = 0;
+	// var timer   = null,
+	var t_start = 0;
 
 	mustRun = mustRun || 100;
 	return function() {
@@ -76,4 +97,21 @@ function throttle(fn, delay, mustRun) {
 			}
 		}, delay);
 	};
+}
+
+
+/**
+ *  =set style transform
+ *  @about    设置元素向左右滑动的样式
+ *
+ *  @param    {dom}     ele   需要设置的元素
+ *  @param    {number}  wid   左右滑动的大小
+ *  @param    {boolean} delay 延迟的时间
+ */
+function setStyle(ele, wid, delay) {
+	delay = delay || 0;
+	ele.style.cssText = 'transform: translate3d(' + wid + 'px, 0px, 0px);' +
+					    '-webkit-transform: translate3d(' + wid + 'px, 0px, 0px);' +
+					    'transition-duration: ' + delay + 'ms;' +
+					    '-webkit-transition-duration: ' + delay + 'ms;';
 }
