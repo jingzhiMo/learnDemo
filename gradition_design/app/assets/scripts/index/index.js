f2745df83420fd0dfe31a3255a6abedaae2adddf
jingzhiMo <mojingzhi.mail@gmail.com>
2016-03-03 15:08:38 +0800

    调整代码

--- gradition_design/app/assets/scripts/index/index.js
+++ gradition_design/app/assets/scripts/index/index.js
@@ -15,30 +15,6 @@ indexApp.controller('indexCtrl', ['$scope', '$http', 'event', function($scope, $
 
 			var ul = ele.children()[0];
 			event.move(ul, ul.offsetWidth / 2, 2);
-			// ele.children()[0].addEventListener('touchstart', function() {
-			// 	// console.log(ele);
-			// 	// console.log(scope);
-			// 	console.log('haha');
-			// }, false);
-
-			// ele.bind('touchstart', function(ev) {
-			// 	positionX = ev.touches[0].pageX;
-			// 	console.log(ele);
-			// 	console.log(attr);
-			// 	console.log('start');
-			// 	console.log(ev);
-			// })
-			// .bind('touchmove', function(ev) {
-			// 	// var distanceX = ev.touches[0].pageX - positionX;
-
-			// 	// ele.
-
-			// 	console.log(ev.touches[0].pageX);
-			// 	console.log('move');
-			// })
-			// .bind('touchend', function(ev) {
-			// 	console.log('end');
-			// });
 		}
 	};
 }]);
