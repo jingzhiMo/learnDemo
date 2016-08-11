d61c06892de66a690a2f27112874c33b5070d65e
jingzhiMo <mojingzhi.mail@gmail.com>
2016-08-11 20:34:16 +0800

    能够正常返回

--- pjax/index.js
+++ pjax/index.js
@@ -8,9 +8,15 @@ let pjaxFilter = require('./filter/pjax');
 
 let app = express();
 
-app.use(express.static('view'));
+app.use(express.static('views'));
+app.use(pjaxFilter);
 
 
 let server = app.listen(3000, function() {
     console.log('listen:3000');
+});
+
+app.get('/rank', function(req, res, next) {
+    console.log(typeof req);
+    res.send({a: 1});
 });
