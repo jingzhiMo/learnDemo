33547f5b77405d8c7d990d02b8903fafc88f0182
jingzhiMo <mojingzhi.mail@gmail.com>
2019-11-11 22:55:37 +0800

    perf(array): add two solution

--- leetcode/1094.car-pooling.js
+++ leetcode/1094.car-pooling.js
@@ -4,6 +4,7 @@
  * @param {number} capacity
  * @return {boolean}
  */
+// 方案一：先对数据进行排序
 var carPooling = function(trips, capacity) {
     trips.sort((a, b) => a[1] - b[1])
     let queue = [] // ['end', 'capacity']
@@ -32,3 +33,21 @@ var carPooling = function(trips, capacity) {
 
     return true
 };
+
+// 方案二：遍历存储上下车的点
+var carPooling = function(trips, capacity) {
+    let queue = new Array(1001).fill(0)
+
+    for (let i = 0; i < trips.length; i++) {
+        queue[trips[i][1]] += trips[i][0]
+        queue[trips[i][2]] -= trips[i][0]
+    }
+
+    for (let i = 1; i < 1001; i++) {
+        queue[i] += queue[i - 1]
+
+        if (queue[i] > capacity) return false
+    }
+
+    return true
+}
